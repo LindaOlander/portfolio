5dad277264eab39314ef8f4aae60b8774cc5cf55
Linda Olander <lindaolander@users.noreply.github.com>
2018-10-09 13:44:18 +0200

    Navigation to id fixed

--- src/components/CaseBox/CaseBox.js
+++ src/components/CaseBox/CaseBox.js
@@ -6,12 +6,12 @@ class CaseBox extends React.Component {
   render() {
     return (
 
-        <div className="caseBox">
-          <Link to={`/case/${this.props.id}`}>
-            <h5>{this.props.title}</h5>
-            <img className="caseImageBox" src={this.props.image} alt="Case" />
-          </Link>
-        </div>
+      <div className="caseBox">
+        <Link to={`/case/${this.props.id}`}>
+          <h5>{this.props.title}</h5>
+          <img className="caseImageBox" src={this.props.image} alt="Case" />
+        </Link>
+      </div>
 
     )
   }
--- src/components/CasePage/CasePage.js
+++ src/components/CasePage/CasePage.js
@@ -1,28 +1,39 @@
 import React from "react"
+import { withRouter } from "react-router-dom"
+import productsJson from "../../products.json"
 
 class CasePage extends React.Component {
 
   render() {
+  
+    const id = this.props.match.params.id
+    let project = null
+    productsJson.products.forEach(item => {
+      if(item.id === id){
+        project = item
+      }
+    })
+
     return (
       <div className="casePage">
-        <img className="casePageImage" src={this.props.image} alt="Case" />
+        <img className="casePageImage" src={project.image} alt="Case" />
         <div className="casePageText">
-          <h1>{this.props.title}</h1>
-          <p>{this.props.description}</p>
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
           <button>See it live</button>
         </div>
         <div className="casePageTech">
           <div className="techContent">
             <h4>Code</h4>
-            <p>{this.props.code}</p>
+            <p>{project.code}</p>
           </div>
           <div className="techContent">
             <h4>Toolbox</h4>
-            <p>{this.props.toolbox}</p>
+            <p>{project.toolbox}</p>
           </div>
           <div className="techContent">
             <h4>More</h4>
-            <p>{this.props.more}</p>
+            <p>{project.more}</p>
           </div>
         </div>
         <div className="buttonBox">
@@ -35,4 +46,4 @@ class CasePage extends React.Component {
 
 }
 
-export default CasePage
+export default withRouter(CasePage)
