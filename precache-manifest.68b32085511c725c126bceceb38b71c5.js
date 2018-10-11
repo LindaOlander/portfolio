a6dd4b21cee594ed70c406c6b3e28c90c16532ca
Linda Olander <lindaolander@users.noreply.github.com>
2018-10-11 10:55:35 +0200

    Updates

---
+++ precache-manifest.68b32085511c725c126bceceb38b71c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2a658cbae73f873f4a90",
+    "url": "/portfolio/static/js/main.2a658cba.chunk.js"
+  },
+  {
+    "revision": "4a2e878c0a5e8ec90b1c",
+    "url": "/portfolio/static/js/1.4a2e878c.chunk.js"
+  },
+  {
+    "revision": "2a658cbae73f873f4a90",
+    "url": "/portfolio/static/css/main.9aa5e966.chunk.css"
+  },
+  {
+    "revision": "959622b9f06a6780647ca1e499b838dc",
+    "url": "/portfolio/index.html"
+  }
+];
